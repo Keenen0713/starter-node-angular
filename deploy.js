c2c0d8169c1180935d2e1df48f9e61b3a7fbde5b
Keenen <keenencartwright25@gmail.com>
2021-02-23 20:56:41 -0600

    git commit -m

--- deploy.js
+++ deploy.js
@@ -85,9 +85,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '52.91.117.204',
+      host: '18.208.191.43',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: 'final-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
